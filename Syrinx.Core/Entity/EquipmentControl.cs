2c3c4af7b1f75ec2c17e5dde30702694263f3baa
robertzml <robertzml@126.com>
2020-11-24 19:50:30 +0800

    add required check in entity.

--- Syrinx.Core/Entity/EquipmentControl.cs
+++ Syrinx.Core/Entity/EquipmentControl.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Syrinx.Core.Entity
@@ -13,21 +14,25 @@ namespace Syrinx.Core.Entity
         /// <summary>
         /// 设备序列号
         /// </summary>
+        [Required]
         public string SerialNumber { get; set; }
 
         /// <summary>
         /// 设备类型
         /// </summary>
+        [Required]
         public int DeviceType { get; set; }
 
         /// <summary>
         /// 控制类型
         /// </summary>
+        [Required]
         public int ControlType { get; set; }
 
         /// <summary>
         /// 控制参数
         /// </summary>
+        [Required]
         public int Option { get; set; }
 
         /// <summary>
--- Syrinx.Core/Entity/EquipmentSpecial.cs
+++ Syrinx.Core/Entity/EquipmentSpecial.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,21 +16,25 @@ namespace Syrinx.Core.Entity
         /// <summary>
         /// 设备序列号
         /// </summary>
+        [Required]
         public string SerialNumber { get; set; }
 
         /// <summary>
         /// 设备类型
         /// </summary>
+        [Required]
         public int DeviceType { get; set; }
 
         /// <summary>
         /// 控制类型
         /// </summary>
+        [Required]
         public int ControlType { get; set; }
 
         /// <summary>
         /// 控制参数
         /// </summary>
+        [Required]
         public string Option { get; set; }
         #endregion //Property
     }
